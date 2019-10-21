594e521eca82fe47adf4b951e45af53973e636eb
Rafał Makuta <rafalmakuta@o2.pl>
2019-10-21 07:29:36 +0200

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,29 @@
+import com.google.common.collect.Lists;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.List;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Connection connect = Jsoup.connect("http://www.onet.pl/");
+        try {
+            Document document = connect.get();
+            Elements links = document.select("span.title");
+            for (Element elem : links) {
+                System.out.println(elem.text());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        List<String> names = Lists.newArrayList("Arek", "Czarek", "Darek");
+        List<String> reversed = Lists.reverse(names);
+        System.out.println(reversed);
+    }
+}
